13761cc381e4833484fe07e9e00569bcc139cd0c
leo <12012705@mail.sustech.edu.cn>
2022-10-25 20:36:47 +0800

    adjust directory structure

--- project1/test.sh
+++ project1/test.sh
@@ -1,11 +1,9 @@
 for i in $(seq -w 12)
 do 
-    touch result/result$i.out
-    chmod 777 result/result$i.out
     ./bin/splc ./phase1/test/test_1_r$i.spl>./phase1/result/result$i.out 2>&1
 done
 for i in $(seq -w 12)
 do 
     echo $i
-    diff ./result/result$i.out ./phase1/test/test_1_r$i.out   
+    diff ./phase1/result/result$i.out ./phase1/test/test_1_r$i.out   
 done
